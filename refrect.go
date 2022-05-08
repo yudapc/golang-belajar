653e14bba01aff50407ba7dbe2cf023e610c627e
Yuda Cogati <yuda.pc@gmail.com>
2022-05-08 13:32:07 +0700

    add sample validation library by struct required

--- refrect.go
+++ refrect.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Sample struct {
-	Name string
+	Name string `required:"true" max:"10"`
 }
 
 func ContohReflect() {
@@ -15,4 +15,16 @@ func ContohReflect() {
 	structField := sampleType.Field(0)
 
 	fmt.Println("Sample reflect: ", structField)
+}
+
+// validation library
+func isValid(data interface{}) bool {
+	t := reflect.TypeOf(data)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.Tag.Get("required") == "true" {
+			return reflect.ValueOf(data).Field(i).Interface() != ""
+		}
+	}
+	return true
 }
--- refrect_test.go
+++ refrect_test.go
@@ -1,7 +1,20 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRefrect(t *testing.T) {
 	ContohReflect()
+}
+
+func TestValidationValid(t *testing.T) {
+	sample := Sample{Name: "Anwar"}
+	fmt.Println("is valid?", isValid(sample))
+}
+
+func TestValidationInvalid(t *testing.T) {
+	sample := Sample{Name: ""}
+	fmt.Println("is valid?", isValid(sample))
 }
