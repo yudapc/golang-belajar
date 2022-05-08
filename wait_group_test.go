80cd6d23e76b6da6af420fa3085e77d47e9e161b
Yuda Cogati <yuda.pc@gmail.com>
2022-05-08 18:46:03 +0700

    add sample sync wait group

---
+++ wait_group_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func RunAsyncronous(group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+
+	fmt.Println("Hello")
+	time.Sleep(1 * time.Second)
+}
+
+func TestWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 10; i++ {
+		go RunAsyncronous(group)
+	}
+
+	group.Wait()
+
+	fmt.Println("Complete")
+}
