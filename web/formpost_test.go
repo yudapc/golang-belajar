2b29c40658284670d7af4f5088a984cceeb37343
Yuda Cogati <yuda.pc@gmail.com>
2022-05-09 07:05:35 +0700

    add form post sample

---
+++ web/formpost_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	firstName := request.PostForm.Get("firstName")
+	lastName := request.PostForm.Get("lastName")
+	fmt.Fprintf(writer, "%s %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("firstName=Joko&lastName=Setiawan")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/requester", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
